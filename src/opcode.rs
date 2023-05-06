fee2f576772e11162b16c8a661a091528ac8fb5e
Aaron <>
2023-05-07 00:04:33 +0100

    added basic arithmetic loop

--- src/opcode.rs
+++ src/opcode.rs
@@ -2,6 +2,21 @@
 #[derive(Debug, PartialEq)]
 #[repr(u8)]
 pub enum Opcode {
+    /// Loads number into register such that {REGISTER} = {16 BIT NUMBER}\
+    /// LOAD {REGISTER} {16 bit number}
+    LOAD,
+    /// Adds two registers such that {OUT REG} = {IN REG 1} + {IN REG 2}\
+    /// ADD {IN REG 1} {IN REG 2} {OUT REG}
+    ADD,
+    /// Subtracts two registers such that {OUT REG} = {IN REG 1} - {IN REG 2}\
+    /// SUB {IN REG 1} {IN REG 2} {OUT REG}
+    SUB,
+    /// Multiplies two registers such that {OUT REG} = {IN REG 1} * {IN REG 2}\
+    /// MUL {IN REG 1} {IN REG 2} {OUT REG}
+    MUL,
+    /// Divides two registers such that {OUT REG} = {IN REG 1} / {IN REG 2}, with remainder stored\
+    /// DIV {IN REG 1} {IN REG 2} {OUT REG}
+    DIV,
     /// Halt
     HLT,
     /// Illegal opcode
@@ -11,7 +26,12 @@ pub enum Opcode {
 impl From<u8> for Opcode {
     fn from(value: u8) -> Self {
         match value {
-            0 => Opcode::HLT,
+            0 => Opcode::LOAD,
+            1 => Opcode::ADD,
+            2 => Opcode::SUB,
+            3 => Opcode::MUL,
+            4 => Opcode::DIV,
+            6 => Opcode::HLT,
             _ => Opcode::IGL,
         }
     }
--- src/vm.rs
+++ src/vm.rs
@@ -8,6 +8,8 @@ pub struct VM {
     pc: usize,
     /// Program to be executed
     program: Vec<u8>,
+    /// Remainder from previous instruction
+    remainder: u32,
 }
 
 impl Default for VM {
@@ -16,11 +18,13 @@ impl Default for VM {
             registers: [0; 32],
             pc: 0,
             program: vec![],
+            remainder: 0,
         }
     }
 }
 
 impl VM {
+    /// Decodes the opcode at the current PC, and increments PC by one
     fn decode_opcode(&mut self) -> Opcode {
         let opcode = Opcode::from(self.program[self.pc]);
         self.pc += 1;
@@ -28,31 +32,99 @@ impl VM {
         opcode
     }
 
-    /// Main VM loop
-    pub fn run(&mut self) {
-        loop {
-            // make sure PC still in program bounds
-            if self.pc >= self.program.len() {
-                break;
-            }
+    /// Returns the next 8 bits in program, and increments PC by one
+    fn next_8_bits(&mut self) -> u8 {
+        let result = self.program[self.pc];
+        self.pc += 1;
 
-            match self.decode_opcode() {
-                Opcode::HLT => {
-                    println!("HLT encountered");
-                    break;
-                }
-                _ => {
-                    println!("Unrecognized opcode encountered");
-                    break;
-                }
+        result
+    }
+
+    /// Returns the next 16 bits in program, and increments PC by two
+    fn next_16_bits(&mut self) -> u16 {
+        let result = ((self.program[self.pc] as u16) << 8) | (self.program[self.pc + 1] as u16);
+        self.pc += 2;
+
+        result
+    }
+
+    /// Runs VM until completion
+    pub fn run(&mut self) {
+        while !self.execute_instruction() {}
+    }
+
+    /// Runs the VM, executing a single instruction
+    pub fn run_once(&mut self) {
+        self.execute_instruction();
+    }
+
+    /// Executes a single instruction, returning a bool indicating if another instruction can be ran
+    /// afterwards
+    fn execute_instruction(&mut self) -> bool {
+        if self.pc >= self.program.len() {
+            return false;
+        }
+
+        match self.decode_opcode() {
+            Opcode::LOAD => {
+                let register = self.next_8_bits() as usize;
+                let number = self.next_16_bits() as u16;
+
+                self.registers[register] = number as i32;
+            }
+            Opcode::ADD => {
+                let register_1 = self.registers[self.next_8_bits() as usize];
+                let register_2 = self.registers[self.next_8_bits() as usize];
+
+                self.registers[self.next_8_bits() as usize] = register_1 + register_2;
+            }
+            Opcode::SUB => {
+                let register_1 = self.registers[self.next_8_bits() as usize];
+                let register_2 = self.registers[self.next_8_bits() as usize];
+
+                self.registers[self.next_8_bits() as usize] = register_1 - register_2;
+            }
+            Opcode::MUL => {
+                let register_1 = self.registers[self.next_8_bits() as usize];
+                let register_2 = self.registers[self.next_8_bits() as usize];
+
+                self.registers[self.next_8_bits() as usize] = register_1 * register_2;
+            }
+            Opcode::DIV => {
+                let register_1 = self.registers[self.next_8_bits() as usize];
+                let register_2 = self.registers[self.next_8_bits() as usize];
+
+                let (div, rem) = (register_1 / register_2, register_1 % register_2);
+                self.registers[self.next_8_bits() as usize] = div;
+                self.remainder = rem as u32;
+            }
+            Opcode::HLT => {
+                println!("HLT encountered");
+            }
+            _ => {
+                println!("Unrecognized opcode encountered");
             }
         }
+
+        true
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn get_test_vm(program: Vec<u8>) -> VM {
+        let mut registers = [0; 32];
+        registers[0] = 5;
+        registers[1] = 10;
+
+        VM {
+            registers,
+            program,
+            ..Default::default()
+        }
+    }
 
     #[test]
     fn test_create_vm() {
@@ -66,7 +138,7 @@ mod tests {
     #[test]
     fn test_opcode_hlt() {
         let mut test_vm = VM {
-            program: vec![0, 0, 0, 0],
+            program: vec![6, 0, 0, 0],
             ..Default::default()
         };
 
@@ -85,5 +157,51 @@ mod tests {
         test_vm.run();
 
         assert_eq!(test_vm.pc, 1);
+    }
+
+    #[test]
+    fn test_load_opcode() {
+        let mut test_vm = VM {
+            program: vec![0, 0, 1, 244],
+            ..Default::default()
+        };
+
+        test_vm.run();
+
+        assert_eq!(test_vm.registers[0], 500);
+    }
+
+    #[test]
+    fn test_add_opcode() {
+        let mut test_vm = get_test_vm(vec![1, 0, 1, 2]);
+        test_vm.run();
+
+        assert_eq!(test_vm.registers[2], 15);
+    }
+
+    #[test]
+    fn test_sub_opcode() {
+        let mut test_vm = get_test_vm(vec![2, 1, 0, 2]);
+        test_vm.run();
+
+        assert_eq!(test_vm.registers[2], 5);
+    }
+
+    #[test]
+    fn test_mul_opcode() {
+        let mut test_vm = get_test_vm(vec![3, 0, 1, 2]);
+        test_vm.run();
+
+        assert_eq!(test_vm.registers[2], 50);
+    }
+
+    #[test]
+    fn test_div_opcode() {
+        let mut test_vm = get_test_vm(vec![4, 1, 0, 2]);
+        test_vm.registers[1] = 11;
+        test_vm.run();
+
+        assert_eq!(test_vm.registers[2], 2);
+        assert_eq!(test_vm.remainder, 1);
     }
 }
